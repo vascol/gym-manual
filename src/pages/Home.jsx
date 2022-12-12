071824cdbd152bdd6325bd80251878bcc64ed97a
amnesia <bizescol@gmail.com>
2022-12-12 11:15:33 +0200

    add Skeleton and mockAPI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import Categories from "../components/Categories";
-import PizzaBlock from "../components/PizzaBlock";
-import Sort from "../components/Sort";
-import pizzas from "../assets/pizzas.json";
+import React from "react"
+import axios from "axios"
+
+import Categories from "../components/Categories"
+import PizzaBlock from "../components/PizzaBlock/PizzaBlock"
+import Sort from "../components/Sort"
+import Skeleton from "../components/PizzaBlock/Skeleton"
 
 const Home = () => {
+  const [isLoading, setIsLoading] = React.useState(true)
+
+  const [pizzaItems, setPizzaItems] = React.useState([])
+
+  React.useEffect(() => {
+    axios
+      .get("https://63948ccc4df9248eada596f8.mockapi.io/items")
+      .then((res) => {
+        setPizzaItems(res.data)
+        setIsLoading(false)
+      })
+  }, [])
+
   return (
     <div className="content">
       <div className="container">
@@ -14,18 +29,18 @@ const Home = () => {
         </div>
         <h2 className="content__title">Всі піци</h2>
         <div className="content__items">
-          {pizzas.map((obj) => (
-            <PizzaBlock key={obj.id} {...obj} />
-          ))}
+          {isLoading
+            ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
+            : pizzaItems.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
         </div>
 
-        <div className="content__error-info">
+        {/* <div className="content__error-info">
           <h2>Виникла помилка 😕</h2>
           <p>Нажаль, не вдалося отримати піци. Спробуйте пізніше.</p>
-        </div>
+        </div> */}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NotFound = () => {
+  return (
+    <div className="content__error-info">
+      <h2>Виникла помилка 😕</h2>
+      <p>Нажаль, не вдалося отримати піци. Спробуйте пізніше.</p>
+    </div>
+  )
+}
+
+export default NotFound
