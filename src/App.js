3edb3c80da2f69ff8cc316c70f1e30014fe306e2
amnesia <bizescol@gmail.com>
2022-12-12 11:14:28 +0200

    add mockAPI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './scss/app.scss'
+import "./scss/app.scss"
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom"
 
-import Home from './pages/Home'
-import Cart from './pages/Cart'
-import MainLayout from './layouts/MainLayout'
+import Home from "./pages/Home"
+import Cart from "./pages/Cart"
+import MainLayout from "./layouts/MainLayout"
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<MainLayout />}>
-          <Route path='' element={<Home />} />
-          <Route path='cart' element={<Cart />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
         </Route>
-
       </Routes>
     </div>
   )
